8fa3ba37e2b8c624dab104165a6ad7a171d87aad
Benjamin R. Bray <benrbray@umich.edu>
2020-07-18 02:32:01 -0400

    copy project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.tsx',
-  devtool: "inline-source-map",
-  mode: "development",
-  module: {
+	entry: './src/index.ts',
+	devtool: "inline-source-map",
+	mode: "development",
+	module: {
 	rules: [
 		{
 			test: /\.ts$/,
